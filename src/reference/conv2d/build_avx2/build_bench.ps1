11abb3f7aa1ec2210a8a01ec388a983288de80be
chenchangguo <chenchangguo@qiuguojihua.com>
2026-08-14 22:05:24 +0800

    Exclude src/reference from repository

--- src/reference/conv2d/build_avx2/build_bench.ps1
+++
@@ -1,4 +0,0 @@
-& "C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\Launch-VsDevShell.ps1" -Arch amd64 -HostArch amd64 -SkipAutomaticLocation *> "D:\aicode\aicode\conv2d\build_avx2\devshell.log"
-Set-Location "D:\aicode\aicode\conv2d"
-& cl /nologo /O2 /arch:AVX2 /utf-8 /W3 "conv2d_avx2_bench.c" "conv2d_ref.c" "conv2d_v1.c" "conv2d_v2.c" "conv2d_v3.c" "conv2d_v4.c" "conv2d_v5.c" "conv2d_v6.c" "/Fe:build_avx2\bench.exe" "/Fo:build_avx2\" *> "D:\aicode\aicode\conv2d\build_avx2\bench_build.log"
-"EXITCODE=$LASTEXITCODE" | Out-File -Append "D:\aicode\aicode\conv2d\build_avx2\bench_build.log"
--- src/reference/conv2d/build_avx2/run_bench.ps1
+++
@@ -1,9 +0,0 @@
-# 运行 bench：PowerShell 侧强制 UTF-8 编解码，确保中文表头正确显示
-[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-$OutputEncoding = [System.Text.Encoding]::UTF8
-Set-Location "D:\aicode\aicode\conv2d\build_avx2"
-$lines = & ".\bench.exe" 2>&1
-# 直接输出到控制台（UTF-8 渲染）
-$lines | ForEach-Object { [Console]::Out.WriteLine($_) }
-# 同时保存为 UTF-8 日志，便于后续查看
-$lines | Out-File -Encoding utf8 "D:\aicode\aicode\conv2d\build_avx2\bench_results.log"
