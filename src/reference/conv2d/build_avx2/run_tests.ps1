c038616d2fec2ccbe4acd3f82801b576782bdddf
celerychen2026 <celerychen2026@users.noreply.github.com>
2026-08-14 22:05:24 +0800

    Exclude src/reference from repository

--- src/reference/conv2d/build_avx2/run_tests.ps1
+++
@@ -1,7 +0,0 @@
-Set-Location "D:\aicode\aicode\conv2d\build_avx2"
-$out = ""
-foreach ($v in 2,3,4,5,6) {
-    $r = & ".\test_v$v.exe" 2>&1 | Out-String
-    $out += "########## conv2d_v$v ##########`n" + $r + "`n"
-}
-$out | Out-File -Encoding UTF8 "D:\aicode\aicode\conv2d\build_avx2\test_results.log"
